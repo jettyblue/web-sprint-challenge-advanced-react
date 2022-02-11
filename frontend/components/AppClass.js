9b520a38f76664090515eed28673fc6dc09a72fe
unknown <lindsay.kelly15@gmail.com>
2022-02-11 14:34:53 -0500

    working on state, struggling

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,7 +1,55 @@
 import React from 'react'
+import axios from 'axios';
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      xoffset: 0,
+      yoffset: 0,
+      delta: 10
+    }
+  }
+
+  // moveSqDown = () => {
+  //   this.setState({ yoffset: this.state.yoffset + this.state.delta });
+  // }
+
+  // moveSqUp = () => {
+  //   this.setState({ yoffset: this.state.yoffset - this.state.delta });
+  // }
+
+  // moveSqRight = () => {
+  //   this.setState({ xoffset: this.state.xoffset + this.state.delta });
+  // }
+
+  // moveSqLeft = () => {
+  //   this.setState({ xoffset: this.state.xoffset - this.state.delta });
+  // }
+
+  // componentDidMount() {
+  //   axios.get('http://localhost:9000/api/result')
+  //   .then(res => {
+  //     console.log(res);
+  //     this.setState({
+  //       ...this.state,
+  //       todos: res.data.data,
+  //     })
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //   })
+  // }
+
+  reset = () => {
+    this.setState({
+      ...this.state
+    })
+  }
+
   render() {
+    // console.log(this.props)
+
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
@@ -24,11 +72,11 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.moveSqLeft}>LEFT</button>
+          <button id="up" onClick={this.moveSqUp}>UP</button>
+          <button id="right"onClick={this.moveSqRight}>RIGHT</button>
+          <button id="down" onClick={this.moveSqDown}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 export default function AppFunctional(props) {
+  const [grid, setGrid] = useState('');
+
+  // const posArray = [
+  //   [0, 0, 0],
+  //   [0, 0, 0],
+  //   [0, 0, 0]
+  // ]
+
+  // posArray[2][0]
+
+  const handleChange = (e) => {
+    setGrid(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.handleAddTodo(grid);
+  }
+
+  // console.log(AppFunctional);
   return (
-    <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+    <>
+      <div id="wrapper" className={props.className}>
+        <div className="info">
+          <h3 id="coordinates">Coordinates (2, 2)</h3>
+          <h3 id="steps">You moved 0 times</h3>
+        </div>
+        <div id="grid">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square active">B</div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+        </div>
+        <div className="info">
+          <h3 id="message"></h3>
+        </div>
+        <div id="keypad">
+          <button id="left" onClick={handleChange}>LEFT</button>
+          <button id="up">UP</button>
+          <button id="right">RIGHT</button>
+          <button id="down">DOWN</button>
+          <button id="reset">reset</button>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input id="email" onChange={handleChange} type="email" placeholder="type email"></input>
+          <input id="submit" type="submit"></input>
+        </form>
       </div>
-      <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-      </div>
-      <div className="info">
-        <h3 id="message"></h3>
-      </div>
-      <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
-      </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
-      </form>
-    </div>
+    </>
   )
 }
+
+
