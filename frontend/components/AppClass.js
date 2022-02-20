77a0b2ee37074d64944e02011e4a0efd42601175
unknown <lindsay.kelly15@gmail.com>
2022-02-20 16:43:44 -0500

    email input and error are functioning

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -2,18 +2,71 @@ import React from 'react'
 import axios from 'axios';
 
 export default class AppClass extends React.Component {
+  state = {
+    x: 2,
+    y: 2,
+    steps: 0,
+    email: '',
+    message: ''
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault()
+    const payload = {
+      x: this.state.x,
+      y: this.state.y,
+      steps: this.state.steps,
+      email: this.state.email
+    }
+    axios.post('http://localhost:9000/api/result', payload)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          ...this.state,
+          email: '',
+          message: res.data.message
+        })
+      })
+      .catch(err => {
+        console.log(err.response)
+        this.state.email !== '' & this.state.email !== 'foo@bar.baz' ?
+        this.setState({
+          ...this.state, message: 'Ouch: email must be a valid email'
+        }) :
+        this.state.email === 'foo@bar.baz' ?
+        this.setState({
+          ...this.state,
+          email: '',
+          message: err.response.data.message
+        }) :
+        this.setState({
+          ...this.state,
+          message: 'Ouch: email is required'
+        })
+      })
+  }
+
+  handleChange = evt => {
+    this.setState({
+      ...this.state,
+      email: evt.target.value
+    })
+  }
+
+  handleReset = () => {
+    this.setState({
+      ...this.state,
+      x: 2,
+      y: 2,
+      steps: 0,
+      email: '',
+      message: ''
+    })
+  }
+
+
   // constructor(props) {
   //   super(props);
-
-    state = {
-      form: [{ x: 2, y: 2, steps: 0 }],
-      email: '',
-      error: '',
-    }
-  
-
-    // TESTING TO MAKE SURE COMMITS PUSH TO GITHUB
-
 
 
   // moveSqDown = () => {
@@ -46,32 +99,49 @@ export default class AppClass extends React.Component {
   //   })
   // }
 
-  onChange = evt => {
-    const { value, steps } = evt.target;
-    this.setState({ ...this.state, [steps]: value })
-  }
+  // state = initialState;
 
-  onSubmit = evt => {
-    evt.preventDefault()
-    const payload = { email: this.state.email };
-    axios.post('http://localhost:9000/api/result', payload)
-      .then(res => {
-        console.log(res)
-        this.setState({ ...this.state, form: res.data.email })
-      })
-      .catch(err => {
-        console.error(err);
-      })
-  }
+  // setActiveSq = (x, y) => {
+  //   const grid = document.getElementsByClassName('square');
 
-  reset = () => {
-    this.setState({ 
-      ...this.state,
-      form: [{ x: 2, y: 2, steps: 0 }],
-      email: '',
-      error: '',
-    })
-  }
+  //   for(let i = 0; i < grid.length; i++) {
+  //     if(grid[i].classList.contains('active')) {
+  //       grid[i].classList.remove('active');
+  //     }
+  //     if(grid[i].textContent) {
+  //       grid[i].textContent = '';
+  //     }
+  //   }
+  // }
+
+
+
+  // onChange = evt => {
+  //   const { value, steps } = evt.target;
+  //   this.setState({ ...this.state, [steps]: value })
+  // }
+
+  // onSubmit = evt => {
+  //   evt.preventDefault()
+  //   const payload = { email: this.state.email };
+  //   axios.post('http://localhost:9000/api/result', payload)
+  //     .then(res => {
+  //       console.log(res)
+  //       this.setState({ ...this.state, form: res.data.email })
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     })
+  // }
+
+  // reset = () => {
+  //   this.setState({ 
+  //     ...this.state,
+  //     form: [{ x: 2, y: 2, steps: 0 }],
+  //     email: '',
+  //     error: '',
+  //   })
+  // }
 
   render() {
     // console.log(this.props)
@@ -80,22 +150,23 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
+          <h3 id="steps">You moved {this.state.steps} time(s)</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+
           <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
+          <div className="square active">B</div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
           <button onClick={this.onChange} id="left">LEFT</button>
@@ -104,8 +175,8 @@ export default class AppClass extends React.Component {
           <button onClick={this.onChange} id="down">DOWN</button>
           <button onClick={this.reset} id="reset">reset</button>
         </div>
-        <form onSubmit={this.onSubmit}>
-          <input onChange={this.onChange} value={this.state.email} id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.email} id="email" type="email" placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
